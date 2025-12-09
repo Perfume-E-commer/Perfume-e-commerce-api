a66966a9f7bb5f21d92b6b53134687252cdc1ef3
LOU Chumdararith <louchumdararith02@gmail.com>
2025-12-09 07:07:20 +0700

    feat: implement inventory logging for sales

---
+++ Perfume_E-commerce/src/main/java/com/Perfume_e_commerce/Repositories/InventoryLogRepository.java
@@ -0,0 +1,4 @@
+package com.Perfume_e_commerce.Repositories;
+
+public interface InventoryLogRepository {
+}
---
+++ Perfume_E-commerce/src/main/java/com/Perfume_e_commerce/models/Inventory/InventoryLog.java
@@ -0,0 +1,4 @@
+package com.Perfume_e_commerce.models.Inventory;
+
+public class InventoryLog {
+}
