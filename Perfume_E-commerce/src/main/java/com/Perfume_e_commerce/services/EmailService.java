3bbf14474d2615e364fd71547f59487d5f4fc9f0
LOU Chumdararith <louchumdararith02@gmail.com>
2026-01-11 11:42:52 +0700

    feat: add sendPasswordResetEmail method to EmailService

--- Perfume_E-commerce/src/main/java/com/Perfume_e_commerce/services/EmailService.java
+++ Perfume_E-commerce/src/main/java/com/Perfume_e_commerce/services/EmailService.java
@@ -18,4 +18,16 @@ public class EmailService {
 
         mailSender.send(message);
     }
+
+    public void sendPasswordResetEmail(String to, String code) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("Reset Your Password - Perfume Shop");
+        message.setText("We received a request to reset your password.\n\n" +
+                "Your password reset code is: " + code + "\n\n" +
+                "If you did not request this, please ignore this email.\n" +
+                "This code expires in 5 minutes.");
+
+        mailSender.send(message);
+    }
 }
